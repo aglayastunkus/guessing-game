a75fc600954ce6cb8642e4c69fc958628b1ec6f5
aglaya <aglayastunkus1@gmail.com>
2021-07-31 22:58:03 +0300

    feat: implement binery search

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.range = [];
+        this.low = 0;
+        this.high = 0;
+        this.mid = 0;
+    }
     setRange(min, max) {
-
+        this.range = Array.from({length: max- min+1}, (v, k) => k);
+        this.low = min;
+        this.high = max;
     }
-
     guess() {
-
+        this.mid = Math.round((this.low + this.high) / 2);
+        return this.range[this.mid];
     }
-
     lower() {
-
+        this.high = this.mid;
     }
-
     greater() {
-
+        this.low = this.mid;
     }
 }
-
 module.exports = GuessingGame;
